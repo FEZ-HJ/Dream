01746d35609c575278172750deafae8abca92522
FEZ-HJ <fez-hj@163.com>
2019-11-28 22:49:11 +0800

    Initial commit

---
+++ dream-common/src/main/java/com/dream/common/exception/user/UserBlockedException.java
@@ -0,0 +1,16 @@
+package com.dream.common.exception.user;
+
+/**
+ * 用户锁定异常类
+ * 
+ * @author dream
+ */
+public class UserBlockedException extends UserException
+{
+    private static final long serialVersionUID = 1L;
+
+    public UserBlockedException()
+    {
+        super("user.blocked", null);
+    }
+}
